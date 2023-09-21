f84b0dfc7162d188f254dfcb7ec83292537b1296
bradbn <98506996+bradbn@users.noreply.github.com>
2023-09-22 07:57:39 +1000

    Update pelms_menu_demo.py

--- bin/pelms_menu_demo.py
+++ bin/pelms_menu_demo.py
@@ -15,6 +15,15 @@ from PIL import ImageTk
 
 
 class PelmsPicamera2Class(picamera2.Picamera2):
+    """Summary of class here.
+
+    Longer class information...
+    Longer class information...
+
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
 
     def __init__(self):
         super().__init__()
@@ -36,8 +45,22 @@ class PelmsPicamera2Class(picamera2.Picamera2):
 
 
 class PelmsTkGuiClass(tk.Tk):
+    """Summary of class here.
+
+    Longer class information...
+    Longer class information...
+
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
 
     def __init__(self):
+        """Initializes the instance based on spam preference.
+
+        Args:
+          likes_spam: Defines if instance exhibits this preference.
+        """
         super().__init__()
         self.geometry('800x480')
         self.resizable(False, False)
@@ -83,6 +106,36 @@ class PelmsTkGuiClass(tk.Tk):
 
         
     def config_Frame_header(self):
+    """Fetches rows from a Smalltable.
+
+    Retrieves rows pertaining to the given keys from the Table instance
+    represented by table_handle.  String keys will be UTF-8 encoded.
+
+    Args:
+      table_handle:
+        An open smalltable.Table instance.
+      keys:
+        A sequence of strings representing the key of each table row to
+        fetch.  String keys will be UTF-8 encoded.
+      require_all_keys:
+        If True only rows with values set for all keys will be returned.
+
+    Returns:
+      A dict mapping keys to the corresponding table row data
+      fetched. Each row is represented as a tuple of strings. For
+      example:
+
+      {b'Serak': ('Rigel VII', 'Preparer'),
+       b'Zim': ('Irk', 'Invader'),
+       b'Lrrr': ('Omicron Persei 8', 'Emperor')}
+
+      Returned keys are always bytes.  If a key from the keys argument is
+      missing from the dictionary, then that row was not found in the
+      table (and require_all_keys must have been False).
+
+    Raises:
+      IOError: An error occurred accessing the smalltable.
+    """
         label_title = tk.Label(self.Frame_header)
         label_title.configure(text=self.title_string, 
             font=('Helvetica', 15, 'bold')
